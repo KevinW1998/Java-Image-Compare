2f1b364c05a839a8ead17a4ba478f57fbec27b67
Your Name <larry.liu@aol.de>
2015-02-02 21:39:28 +0100

    Signed-off-by: Your Name <larry.liu@aol.de>

--- Image-Compare/src/org/kevsoft/test/TestClass.java
+++ Image-Compare/src/org/kevsoft/test/TestClass.java
@@ -1,9 +1,12 @@
 package org.kevsoft.test;
 
 public class TestClass {
+	public static void test () {
+		System.out.println ("hallo wie gehts");
+	}
 
 	public static void main(String[] args) {
-		
+		test ();
 	}
 
 }
