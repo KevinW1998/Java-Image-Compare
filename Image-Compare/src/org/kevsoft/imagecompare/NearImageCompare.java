b74fde6e828fa05ca99ec1fad9337f501cb052f8
Your Name <larry.liu@aol.de>
2015-02-14 22:56:11 +0100

    Signed-off-by: Your Name <larry.liu@aol.de>

--- Image-Compare/src/org/kevsoft/imagecompare/NearImageCompare.java
+++ Image-Compare/src/org/kevsoft/imagecompare/NearImageCompare.java
@@ -19,11 +19,32 @@ public class NearImageCompare extends ImageCompare {
 		int pixels2[] = new int[wImg2.getWidth()*wImg2.getHeight()*4];
 		wImg2.getPixels(0, 0, wImg2.getWidth(), wImg2.getHeight(), pixels2);
 		
-		
-		double durchschnittRot;
-		double durchschnittGrün;
-		double durchschnittBlau;
-		
-		return 0; //Im moment 0
+		  double anzahlDerPxImg = wImg1.getWidth()*wImg1.getHeight();
+		  double durchschnittRot=0;
+		  double durchschnittGrün=0;
+		  double durchschnittBlau=0;
+		  double durchschnittRot1=0;
+		  double durchschnittGrün1=0;
+		  double durchschnittBlau1=0;
+		  
+		  for (int i = 0; i< pixels1.length; i= i+ 4 ){
+		   durchschnittRot=durchschnittRot + pixels1[i];
+		   durchschnittRot1=durchschnittRot1 + pixels2[i];
+		  }
+		  for (int x = 1; x< pixels1.length; x= x+ 4 ){
+		   durchschnittGrün=durchschnittGrün + pixels1[x];
+		   durchschnittGrün1=durchschnittGrün1 + pixels2[x];
+		  }
+		  for (int y= 1; y< pixels1.length; y= y+ 4 ){
+		   durchschnittBlau=durchschnittBlau + pixels1[y];
+		   durchschnittBlau1=durchschnittBlau1 + pixels2[y];
+		  }
+		  durchschnittRot = durchschnittRot / anzahlDerPxImg;
+		  durchschnittGrün = durchschnittGrün / anzahlDerPxImg;
+		  durchschnittBlau = durchschnittBlau / anzahlDerPxImg;
+		  durchschnittRot1 = durchschnittRot1 / anzahlDerPxImg;
+		  durchschnittGrün1 = durchschnittGrün1 / anzahlDerPxImg;
+		  durchschnittBlau1 = durchschnittBlau1 / anzahlDerPxImg;
+		  return 0; //Im moment 0
 	}
 }
