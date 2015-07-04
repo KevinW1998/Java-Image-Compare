5d991e2e8de86843545445e0c2979e195e67574f
Larry <larry.liu@aol.de>
2015-07-04 11:35:59 +0200

    Buffered Writer hinzugefügt

--- Image-Compare/src/org/kevsoft/test/TestClass2.java
+++ Image-Compare/src/org/kevsoft/test/TestClass2.java
@@ -1,8 +1,13 @@
 package org.kevsoft.test;
 
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.ArrayList.*;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
@@ -10,7 +15,8 @@ import org.kevsoft.imagecompare.PdiffImageCompare;
 
 public class TestClass2 {
 
-	public static void test(String imagePfadA, String imagePfadB) {
+	public static void test(String imagePfadA, String imagePfadB, String ausgabedateipfad) throws IOException {
+		ArrayList<Double> liste = new ArrayList<Double>();
 		double d = 0;
 		while (d < 89) {
 			PdiffImageCompare pdiff;   
@@ -27,13 +33,22 @@ public class TestClass2 {
 			   }
 			   pdiff.setFov(d);
 			double erg = pdiff.compare();
+			liste.add (erg);
+			
+			
+			BufferedWriter bw = new BufferedWriter(new FileWriter(ausgabedateipfad));
+			bw.write("hallo");
+			for (int i=0; i< liste.size(); i++){
+				bw.write("/n"+liste.get(i) );
+			}
+			
 			d= d+ 0.1;
-			System.out.println(erg);
+			
 		}
 
 	}
 
-	public static void main(String args[]) {
-		test("motorrad.jpg", "motorrad.jpg");
+	public static void main(String args[]) throws IOException {
+		test("motorrad.jpg", "motorrad.jpg","./Users/larry/Documents/Entwickler Umgebung/java/ausgabe.csv");
 	}
 }
