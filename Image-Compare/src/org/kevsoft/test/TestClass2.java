c4adc28cd4f6fb98e2e6bd97d6e81e55eccc6f2d
larry_liu <larry.liu@aol.de>
2015-07-07 15:39:28 +0200

    Testklasse 3 fertiggestellt

--- Image-Compare/src/org/kevsoft/test/TestClass2.java
+++ Image-Compare/src/org/kevsoft/test/TestClass2.java
@@ -16,55 +16,106 @@ import javax.imageio.ImageIO;
 import org.kevsoft.imagecompare.PdiffImageCompare;
 
 public class TestClass2 {
-	
-	public static void test(String imagePfadA, String imagePfadB) throws IOException {
+
+	public static void testSingle(String imagePfadA, String imagePfadB, String ausgabedateipfad) throws IOException {
+		ArrayList<Double> liste = new ArrayList<Double>();	
+		ArrayList<Double> liste1 = new ArrayList<Double>();
+		ArrayList<Double> liste2 = new ArrayList<Double>();
+
+		BufferedWriter bw = new BufferedWriter(new FileWriter(ausgabedateipfad));
+		bw.write("fov;colorfactor 0;colorfactor 0.5;colorfactor 1");
+		int d= 1;
+		while ( d < 90) {
+
+			PdiffImageCompare pdiff;
+			BufferedImage img1;
+			BufferedImage img2;
+			try {
+				img1 = ImageIO.read(new File(imagePfadA));
+				img2 = ImageIO.read(new File(imagePfadB));
+				pdiff = new PdiffImageCompare(img1, img2);
+
+				pdiff.setFov(d);
+				pdiff.setSizeScale(new Dimension(600, 450));
+				
+				pdiff.setColorfactor(0);
+				double speicher = pdiff.comparePercent();			
+				liste.add(speicher);
+				
+				pdiff.setColorfactor(0.5);
+				double speicher1 = pdiff.comparePercent();
+				liste1.add(speicher1);
+				
+				pdiff.setColorfactor(1);
+				double speicher2 = pdiff.comparePercent();
+				liste2.add(speicher2);
+				
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				return;
+			}
+			
+			d++;
+		}
+		for (int z = 0; z<liste.size(); z++){
+			int fov = z+1;
+			String reihe1 = ""+liste.get(z);
+			reihe1= reihe1.replace('.',',');
+			
+			String reihe2 = ""+liste1.get(z);
+			reihe2= reihe2.replace('.',',');
+			
+			String reihe3 = ""+liste2.get(z);
+			reihe3= reihe3.replace('.',',');
+			
+			
+			bw.write("\n"+ fov+";"+ reihe1 +";" + reihe2  +";" + reihe3  );
+		}
+		bw.close();
+	}
+
+	public static void test(String imagePfadA, String imagePfadB, String ausgabedateipfad) throws IOException {
 		ArrayList<Double> liste = new ArrayList<Double>();
 		ArrayList<PdiffImageCompare> pa = new ArrayList<PdiffImageCompare>();
 		HashMap map = new HashMap();
-//		BufferedWriter bw = new BufferedWriter(new FileWriter(ausgabedateipfad));
-		
-		for (int d =1 ; d < 90;d++) {
-			
-//			PdiffImageCompare pdiff;   
-			   BufferedImage img1;
-			   BufferedImage img2;
-			   try {
-				   img1 = ImageIO.read(new File(imagePfadA));
-				   img2 = ImageIO.read(new File(imagePfadB));
-//				   pdiff = new PdiffImageCompare(img1, img2);
-				   PdiffImageCompare p= new PdiffImageCompare (img1, img2);
-				   p.setFov(d);
-				   p.setSizeScale(new Dimension (600,450));
-				   pa.add(p);
-				  
-				   
-//				   pdiff.setSizeScale(new Dimension(600, 450));
-			   }catch(Exception e) {
-				   e.printStackTrace();
-		    	   return;
-			   }
-//			   pdiff.setFov(d);
-//			double erg = pdiff.compareMultipleParallel();
-//			liste.add (erg);
-//			System.out.println(erg);
-			
-			 
+
+
+		for (int d = 1; d < 90; d++) {
+
+			// PdiffImageCompare pdiff;
+			BufferedImage img1;
+			BufferedImage img2;
+			try {
+				img1 = ImageIO.read(new File(imagePfadA));
+				img2 = ImageIO.read(new File(imagePfadB));
+				// pdiff = new PdiffImageCompare(img1, img2);
+				PdiffImageCompare p = new PdiffImageCompare(img1, img2);
+				p.setFov(d);
+				p.setSizeScale(new Dimension(600, 450));
+				pa.add(p);
+
+				// pdiff.setSizeScale(new Dimension(600, 450));
+			} catch (Exception e) {
+				e.printStackTrace();
+				return;
+			}
+			// pdiff.setFov(d);
+			// double erg = pdiff.compareMultipleParallel();
+			// liste.add (erg);
+			// System.out.println(erg);
+
 		}
-		HashMap <PdiffImageCompare, Integer> hm = PdiffImageCompare.compareMultipleParallel(pa); 
-		for (int i=0; i<pa.size();i++ ){
-			
-			int pixelsfailed= hm.get(pa.get(i)).intValue();
+		HashMap<PdiffImageCompare, Integer> hm = PdiffImageCompare.compareMultipleParallel(pa);
+		for (int i = 0; i < pa.size(); i++) {
+
+			int pixelsfailed = hm.get(pa.get(i)).intValue();
 			System.out.println(pixelsfailed);
 		}
-		 
-			
-			
-			
-		}
 
-	
+	}
 
 	public static void main(String args[]) throws IOException {
-		test("wohnung1.jpg", "wohnung2.jpg");
+		testSingle("wohnung1.jpg", "wohnung2.jpg","E:\\Irian\\Image-Compare\\ausgabedatei.csv");
 	}
 }
